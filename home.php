b92c3ec4d60d60b71ea545f8ab8eed0687ab81b8
realuzzi <100102183+realuzzi@users.noreply.github.com>
2023-01-11 17:46:53 -0600

    Update home.php

--- home.php
+++ home.php
@@ -17,8 +17,28 @@
 
 	<?php include('includes/aside.inc');?>
 	
-	<section>
-	<?php include 'dbc.php';?>
+		<section>
+	<?php 
+		include('includes/dbc.php');
+		
+		$query ="SELECT * FROM home_page ORDER BY id DESC";
+		$result = mysqli_query($con,$query) ; // $con from dbc.php
+		if($result == false){
+			$error_message = mysqli_error();
+			echo "<p>There has been a query error; $error_message</p>";
+		}
+		if(mysqli_num_rows($result)==0) {
+			echo "No content is available at this time. Please check back soon.";
+		}
+		while($row=mysqli_fetch_assoc($result)){
+			echo '<h2>'.$row['title].'</h2>';
+			echo '<p>'.$row['message'].'</p>';
+		}	
+		mysqli_free_result($result);
+		mysqli_close($con);
+		
+	?>	
+		include ('includes/dbc_admin.php');?>
 	<p>Main content goes here...</p>
 	<p>Main content goes here...</p>
 	<p>Main content goes here...</p>
